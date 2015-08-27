e8ea223b868b614542a68e362eaf145beb647676
j <i>
2015-08-27 19:24:06 +0200

    nodo admi

--- WebserviceTransmetro/src/java/Arbolavl/Administrador.java
+++ WebserviceTransmetro/src/java/Arbolavl/Administrador.java
@@ -10,5 +10,27 @@ package Arbolavl;
  * @author javier
  */
 public class Administrador {
-    
+    int id;
+    String correo;
+    String contra;
+    Comparable dato;
+    int fe;
+    Administrador hijoIzquierdo;
+    Administrador hijoDerecho;
+    public Administrador(String correo,String contra) {
+        this.fe = 0;
+        this.correo=correo;
+        this.contra=contra;
+        this.hijoDerecho=null;
+        this.hijoIzquierdo=null;
+        this.id=tamaContra(contra);
+    }
+    private int tamaContra(String dato){
+        int c=0;
+        for (int x=0;x<dato.length();x++){
+              c=c+dato.codePointAt(x);
+        }
+        System.out.println(c);
+        return c;
+    }
 }
---
+++ WebserviceTransmetro/src/java/Arbolavl/ArbolAdmin.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arbolavl;
+
+/**
+ *
+ * @author javier
+ */
+public class ArbolAdmin {
+    private Administrador raiz;
+    
+    public ArbolAdmin() {
+      this.raiz=null;
+    }
+    
+    
+    
+}
